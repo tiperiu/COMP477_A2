afb61f7df6158adb2e568bcdbe9c9d811ee5e801
Maksym Perepichka <max@maxedoutstudio.com>
2021-11-03 10:35:50 -0400

    Fixes to main files (2)

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <string>
 
-void test_splines();
 
 
 using namespace std;
@@ -14,9 +13,6 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     MainWindow w;
-
-    // I have different applications
-    // they can be switched in te constructor of the ogl widget
 
     w.show();
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,9 +3,9 @@
 
 #include "dependencies/json.hpp"
 
-#include "OpenGL/widgets/A1_2Dwidget.h"
-#include "OpenGL/layers/A1_2dlayer.h"
-#include "solution/a1solution.h"
+#include "OpenGL/widgets/A2_2Dwidget.h"
+#include "OpenGL/layers/A2_2dlayer.h"
+#include "solution/a2solution.h"
 
 
 #include <QFileDialog>
@@ -62,8 +62,8 @@ void MainWindow::on_actionLoad_triggered()
     QString json_data = in.readAll();
     json j = json::parse(json_data.toStdString());
 
-    A1_2DWidget* w = ui->OpenGL;
-    A1_2DLayer* l = dynamic_cast<A1_2DLayer*>(w->m_layers[0]);
+    A2_2DWidget* w = ui->OpenGL;
+    A2_2DLayer* l = dynamic_cast<A2_2DLayer*>(w->m_layers[0]);
 
     // Deletes existing structures
     for(unsigned int i =0; i<l->m_joints.size(); i++){
@@ -73,6 +73,12 @@ void MainWindow::on_actionLoad_triggered()
     }
     l->m_joints.clear();
     l->m_links.clear();
+    for(unsigned int i =0; i<l->m_obstacles.size(); i++){
+        // Note, deleting a joint also deletes any associated links
+        delete l->m_obstacles[i];
+        l->m_obstacles[i] = nullptr;
+    }
+    l->m_obstacles.clear();
 
 
     // Logic for deserialization
@@ -84,6 +90,7 @@ void MainWindow::on_actionLoad_triggered()
         Joint2D* joint = new Joint2D(l->m_parent);
         joint->m_id = (*itr)["id"].get<std::string>();
         id_map[joint->m_id] = joint;
+        joint->set_locked((*itr)["locked"]);
         QVector2D pos((*itr)["pos_x"], (*itr)["pos_y"]);
         joint->set_position(pos);
         l->m_joints.push_back(joint);
@@ -96,8 +103,20 @@ void MainWindow::on_actionLoad_triggered()
         link->compute(QVector2D(0,0));
         l->m_links.push_back(link);
     }
+    for (auto itr = j["obstacles"].begin(); itr!= j["obstacles"].end(); itr++){
+        Obstacle2D* obstacle = new Obstacle2D(l->m_parent);
+        obstacle->m_id = (*itr)["id"];
+        QVector2D pos((*itr)["pos_x"], (*itr)["pos_y"]);
+        obstacle->compute(pos, 20, 100);
+        obstacle->m_color = QVector3D(1,0,0);
+        l->m_obstacles.push_back(obstacle);
+    }
+
+
 
     if(w->m_layers.size()!=0){
+
+
     }
     w->update();
 }
@@ -121,8 +140,8 @@ void MainWindow::on_actionSave_triggered()
     // Logic for serialization
     json j;
 
-    A1_2DWidget* w = ui->OpenGL;
-    A1_2DLayer* l = dynamic_cast<A1_2DLayer*>(w->m_layers[0]);
+    A2_2DWidget* w = ui->OpenGL;
+    A2_2DLayer* l = dynamic_cast<A2_2DLayer*>(w->m_layers[0]);
 
     j["joints"] = json::array();
     j["links"] = json::array();
@@ -135,6 +154,7 @@ void MainWindow::on_actionSave_triggered()
         saved_joint["pos_x"] = pos.x();
         saved_joint["pos_y"] = pos.y();
         saved_joint["id"] = joint->m_id;
+        saved_joint["locked"] = joint->is_locked();
         j["joints"].push_back(saved_joint);
     }
     for(unsigned int i =0; i<l->m_links.size(); i++){
@@ -144,6 +164,15 @@ void MainWindow::on_actionSave_triggered()
         saved_link["first_joint_id"] = link->get_first_joint()->m_id;
         saved_link["second_joint_id"] = link->get_second_joint()->m_id;
         j["links"].push_back(saved_link);
+    }
+    for(unsigned int i =0; i<l->m_obstacles.size(); i++){
+        Obstacle2D* obstacle = l->m_obstacles[i];
+        json saved_obstacle = json::object();
+        QVector2D pos = obstacle->m_center;
+        saved_obstacle["pos_x"] = pos.x();
+        saved_obstacle["pos_y"] = pos.y();
+        saved_obstacle["id"] = obstacle->m_id;
+        j["obstacles"].push_back(saved_obstacle);
     }
 
     // Completes the saving process
@@ -156,7 +185,7 @@ void MainWindow::on_actionSave_triggered()
 
 void MainWindow::on_actionReset_View_triggered()
 {
-    A1_2DWidget* w = ui->OpenGL;
+    A2_2DWidget* w = ui->OpenGL;
     if(w->m_layers.size()!=0){
         w->m_layers[0]->reset_view();
     }
@@ -166,7 +195,7 @@ void MainWindow::on_actionReset_View_triggered()
 
 void MainWindow::on_actionSelect_and_Edit_Joints_triggered()
 {
-    A1_2DWidget* w = ui->OpenGL;
+    A2_2DWidget* w = ui->OpenGL;
     if(w->m_layers.size()!=0){
         w->m_layers[0]->setUIMode(1);
     }
@@ -177,7 +206,7 @@ void MainWindow::on_actionSelect_and_Edit_Joints_triggered()
 
 void MainWindow::on_actionSelect_and_Edit_Links_triggered()
 {
-    A1_2DWidget* w = ui->OpenGL;
+    A2_2DWidget* w = ui->OpenGL;
     if(w->m_layers.size()!=0){
         w->m_layers[0]->setUIMode(2);
     }
@@ -187,7 +216,7 @@ void MainWindow::on_actionSelect_and_Edit_Links_triggered()
 
 void MainWindow::on_actionSelect_and_Edit_Obstacles_triggered()
 {
-    A1_2DWidget* w = ui->OpenGL;
+    A2_2DWidget* w = ui->OpenGL;
     if(w->m_layers.size()!=0){
         w->m_layers[0]->setUIMode(3);
     }
@@ -197,7 +226,7 @@ void MainWindow::on_actionSelect_and_Edit_Obstacles_triggered()
 
 void MainWindow::on_actionShowtime_triggered()
 {
-    A1_2DWidget* w = ui->OpenGL;
+    A2_2DWidget* w = ui->OpenGL;
     if(w->m_layers.size()!=0){
         w->m_layers[0]->setUIMode(4);
     }
@@ -208,5 +237,5 @@ void MainWindow::on_actionShowtime_triggered()
 
 void MainWindow::on_actionTest_Eigen_triggered()
 {
-    A1Solution::test_eigen_library();
+    A2Solution::test_eigen_library();
 }
